d11362419d7c614457a9a952a4c03a5ae65fe899
kenz115 <chiguchikentaro@kentaronoMacBook-Pro.local>
2021-06-09 17:50:43 +0900

    Add rooms/index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_115755) do
+ActiveRecord::Schema.define(version: 2021_06_09_071557) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_115755) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "area"
   end
 
 end
